b77d31d9078f2f25c3438139a7d579f04c22b82f
secretmoo35 <sarayut-moo.th@hotmail.com>
2018-04-20 12:50:35 +0700

    update

--- test.js
+++ test.js
@@ -2,8 +2,23 @@
 
 process.env.MONGODB_URI_TEST = 'mongodb://localhost/database-test';
 
+const request = require('supertest');
+const app = require('./src/config/express');
+
 var glob = require('glob'),
     path = require('path');
+
+describe('Server', function () {
+
+    it('should be start server', function (done) {
+        request(app)
+            .get('/')
+            .expect(200)
+            .end(done);
+
+    });
+
+});
 
 glob.sync(path.join(__dirname, './src/modules/**/test/*.js')).forEach(function (file) {
     require(path.resolve(file));
